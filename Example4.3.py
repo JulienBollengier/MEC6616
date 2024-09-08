e0b21f0f7dacce5ae6055697073a264b0a07205b
ChrisSto45 <118450312+ChrisSto45@users.noreply.github.com>
2024-09-08 19:39:09 -0400

    Update Example4.3.py

--- Example4.3.py
+++ Example4.3.py
@@ -5,7 +5,7 @@ from numpy.linalg import solve
 import matplotlib.pyplot as plt
 
 """ Ecrit par """
-""" Julien Bollengier - """
+""" Bollengier Julien - """
 """ Tiboeuf Christopher - 2362869 """
 
 """ Example 4.3 """
