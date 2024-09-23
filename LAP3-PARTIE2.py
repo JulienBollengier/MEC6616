fbb093d703c6f10030305d6ed3c6bf75eb67edb1
Julien <81406693+JulienBollengier@users.noreply.github.com>
2024-09-23 17:43:44 -0400

    Add files via upload

--- LAP3-PARTIE2.py
+++ LAP3-PARTIE2.py
@@ -95,13 +95,16 @@ def Divergence(msh_obj):
 
 """ Fonction permettant de déterminer le gradient au centre des volumes par la méthode des moindres-carrés """
 def Gradient(msh_obj, phi0):
+    ALS = np.zeros((2,2));
+    NTRI = len(msh_obj.element_to_nodes_start);
+    B = np.zeros((2,1));
     for i in range(len(msh_obj.face_to_nodes)):
         # Arrête à l'étude
         arrete = msh_obj.face_to_nodes[i];
         
         # Elements à gauche et à droite de l'arrête étudiée
-        element_gauche = msh_obj.face_to_elements[arrete[0]];
-        element_droite = msh_obj.face_to_elements[arrete[1]];
+        element_gauche = arrete[0];
+        element_droite = arrete[1];
         
         # Indice des noeuds des elements de gauche et de droite de l'arrête
         start_gauche = msh_obj.element_to_nodes_start[element_gauche];
@@ -121,24 +124,35 @@ def Gradient(msh_obj, phi0):
             XCG_droite = 1/3*(msh_obj.node_to_xcoord[noeuds_droite[0]]+msh_obj.node_to_xcoord[noeuds_droite[1]]+msh_obj.node_to_xcoord[noeuds_droite[2]]);
             YCG_droite = 1/3*(msh_obj.node_to_ycoord[noeuds_droite[0]]+msh_obj.node_to_ycoord[noeuds_droite[1]]+msh_obj.node_to_ycoord[noeuds_droite[2]]);
         
+        # Coordonnées du centre de l'arrête
+        if element_droite == -1 :
+            XCG_droite = 1/2*(msh_obj.node_to_xcoord[arrete[0]] + msh_obj.node_to_xcoord[arrete[1]])
+            YCG_droite = 1/2*(msh_obj.node_to_ycoord[arrete[0]] + msh_obj.node_to_ycoord[arrete[1]])
+        
         # DX et DY à gauche et à droite de l'arrête
+        DX_droite = XCG_gauche - XCG_droite;
+        DY_droite = YCG_gauche - YCG_droite;
+            
         DX_gauche = XCG_droite - XCG_gauche;
         DY_gauche = YCG_droite - YCG_gauche;
         
-        if element_droite != -1 :
-            DX_droite = XCG_gauche - XCG_droite;
-            DY_droite = YCG_gauche - YCG_droite;
-            
-        # DX et DY pour les frontières de Dirichlet
+        DPHI_gauche = phi0;
+        
+
+        ALS[0,0] = DX_gauche * DX_gauche;
+        ALS[1,0] = DX_gauche * DY_gauche;
+        ALS[0,1] = DY_gauche * DX_gauche;
+        ALS[1,1] = DY_gauche * DY_gauche;
+        
+        B[0,:] = DX_gauche * DPHI_gauche;
+        B[1,:] = DY_gauche * DPHI_gauche;
+        
+        ATAI = np.linalg.inv(ALS);
+        
+        GRAD = np.dot(ATAI,B)
+        
+    return GRAD
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
+       
         
